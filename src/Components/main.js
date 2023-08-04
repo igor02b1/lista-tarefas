a751f2370f7d1bf13da7756ca824ff91c28dbd95
igor02b1 <igor02b1@gmail.com>
2023-08-04 03:12:07 -0300

    feat: saving task in localstorage

--- src/Components/main.js
+++ src/Components/main.js
@@ -8,11 +8,28 @@ class Main extends Component {
     state = {
         novaTarefa: '',
         tarefas: [],
+        index: -1,
     };
+
+    componentDidMount() {
+        const tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+        if (!tarefas) return;
+
+        this.setState({ tarefas });
+    }
+
+    componentDidUpdate(prevProps, prevStates) {
+        const { tarefas } = this.state;
+
+        if (tarefas === prevStates.tarefas) return;
+
+        localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        const { tarefas } = this.state;
+        const { tarefas, index } = this.state;
         let { novaTarefa } = this.state;
         novaTarefa = novaTarefa.trim();
 
@@ -20,9 +37,20 @@ class Main extends Component {
 
         const novasTarefa = [...tarefas];
 
+        if(index === -1) { 
+            this.setState({
+                tarefas: [...novasTarefa, novaTarefa],
+                novaTarefa: '',
+            });
+        } else {
+        novasTarefa[index] = novaTarefa;    
+
         this.setState({
-            tarefas: [...novasTarefa, novaTarefa]
+            tarefas: [...novasTarefa],
+            index: -1,
         })
+        }
+
     }
 
     handleChange = (e) => {
@@ -32,7 +60,12 @@ class Main extends Component {
     }
 
     handleEdit = (e, index) => {
+        const { tarefas  } = this.state;
 
+        this.setState({
+            index,
+            novaTarefa: tarefas[index],
+        })
     }
 
     handleDelete = (e, index) => {
