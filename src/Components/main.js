40ce23000a4d9b41b10d305af67e1ce7f186bb26
igor02b1 <igor02b1@gmail.com>
2023-08-04 02:23:51 -0300

    feat: edit task funcionality

--- src/Components/main.js
+++ src/Components/main.js
@@ -31,6 +31,20 @@ class Main extends Component {
         });
     }
 
+    handleEdit = (e, index) => {
+
+    }
+
+    handleDelete = (e, index) => {
+        const { tarefas } = this.state;
+        const novasTarefa = [...tarefas];
+        novasTarefa.splice(index, 1);
+
+        this.setState({
+            tarefas: [...novasTarefa],
+        });
+    }
+
 
     render() {
         const { novaTarefa, tarefas } = this.state;
@@ -52,12 +66,17 @@ class Main extends Component {
             </form>
 
             <ul className="tarefas">
-                {tarefas.map((tarefa) => (
+                {tarefas.map((tarefa, index) => (
                     <li key={tarefa}> 
                     {tarefa}
                     <span>
-                        <FaEdit className="edit"/>
-                        <FaWindowClose className="delete"/>
+                        <FaEdit className="edit"
+                        onClick={(e) => this.handleEdit(e, index)}
+                        />
+                        <FaWindowClose 
+                        onClick={(e) => this.handleDelete(e, index)}
+                         className="delete"
+                         />
                     </span>
                     </li>
             ))}
